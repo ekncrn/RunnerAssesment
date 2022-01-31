a0b18d72270cc6c02a2f4588e09a55ca1f2c3de5
ekncrn <ekncrn.erdogan@gmail.com>
2022-01-31 16:33:01 +0300

    TestFolder

---
+++ SumOfLibraryTest.cs
@@ -0,0 +1,28 @@
+﻿using SumOfLibrary;
+using System;
+using Xunit;
+
+namespace TestProject
+{
+    public class SumOfLibraryTest
+    {
+        [Theory]
+        [InlineData(10, 23)]
+        [InlineData(20, 63)]
+        [InlineData(1, 0)]
+        [InlineData(3, 0)]
+        [InlineData(5, 3)]
+        public void SumOfMultiple_Test(int limit,long expected)
+        {
+            ISumOfLibraryService sumOfLibrary = new SumOfLibraryService();
+            Assert.Equal(expected, sumOfLibrary.Do(limit));
+        }
+
+        [Fact]
+        public void SumOfMultiple_ExceptionTest()
+        {
+            ISumOfLibraryService sumOfLibrary = new SumOfLibraryService();
+            Assert.Throws<Exception>(() => sumOfLibrary.Do(-3));
+        }
+    }
+}
